e04bc8c19e3f7a2a2bf4f35406ad49dfdbf350dd
Brijesh Kumar <BrijeshKum@hcl.com>
2019-11-05 16:51:42 +0530

    merge commit

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,58 +1,5 @@
 import React from "react";
-import styled from "styled-components";
-import Label from "../atoms/Label";
-import InputText from "../atoms/InputText";
-import Button from "../atoms/Button";
 import LoginForm from "../template/LoginForm";
-const StyledLogin = styled.div`
-  display: flex;
-  align-items: center;
-  flex-flow: column;
-  width: 350px;
-  height: 350px;
-  margin: 0 auto;
-  border: 2px solid #000;
-  border-radius: 20px;
-  background: #eee;
-  
-
-
-  h2 {
-    font-family: Arial, Helvetica, sans-serif;
-    font-size: 14px;
-  }
-  `;
-
-//   button {
-//     background: green;
-//     color: #fff;
-//     padding: 10px;
-//     margin: 5px;
-//     width: 150px;
-//     border: none;
-//     border-radius: 10px;
-//     box-sizing: border-box;
-//   }
-// `;
-// const StyledLabel = styled.label`
-// font-weight: bold; 
-// caret-color: getColor('blue'); 
-// display: block;
-// text-align: left;
-// margin-right: 23%;
-
-// `;
-
-
-// const StyledInput = styled.input`
-//   border: 1px solid #000;
-//   border-radius: 10px;
-//   padding: 10px;
-//   margin: 5px;
-//   width: 150px;
-//   box-sizing: border-box;
-// `;
-
 
 const Login = () => {
   return (
