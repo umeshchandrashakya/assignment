72471823bf2e779209cd07d64e67e2986125f5ac
Brijesh Kumar <BrijeshKum@hcl.com>
2019-11-05 15:35:29 +0530

    commit before merge

--- src/__tests__/Login.test.tsx
+++ src/__tests__/Login.test.tsx
@@ -3,6 +3,7 @@ import Login from '../components/pages/Login';
 import * as enzyme from 'enzyme';
 //jest.unmock('axios');
 import axios from 'axios';
+import { DefaultTypelessProvider } from 'typeless';
 //import MockAdapter from 'axios-mock-adapter';
 //import { userLogin, getUsers } from '../apis';
 
@@ -17,7 +18,11 @@ describe('<Login />', () => {
     let wrapper: enzyme.ShallowWrapper;
 
     beforeEach(() => {
-        wrapper = enzyme.shallow(<Login />);
+        wrapper = enzyme.shallow(
+            <DefaultTypelessProvider>
+                <Login />
+            </DefaultTypelessProvider>
+        );
     });
 
     it('should call a userLogin function', async () => {
@@ -34,7 +39,7 @@ describe('<Login />', () => {
         const stateCallback = jest.fn();
         const response = await axios.get('https://simple-login-signup-rest.herokuapp.com/api/user');
         //const response = await getUsers();
-        expect(response).toEqual(responseData.data);
+        expect(response).toEqual(responseData);
         expect(mockedAxios.get).toHaveBeenCalledTimes(1);
         expect(mockedAxios.get).toHaveBeenCalledWith('https://simple-login-signup-rest.herokuapp.com/api/user');
         //expect(wrapper.find('success')).not.toBeNull();
@@ -48,17 +53,17 @@ describe('<Login />', () => {
 
         //jest.spyOn(user, '')
 
-        const usernameInput = wrapper.find('input.input1');
+        const usernameInput = wrapper.find('.input1');
         usernameInput.simulate('change', { target: { value: 'brij@hcl.com' } });
 
-        const passwordInput = wrapper.find('input.input2');
+        const passwordInput = wrapper.find('.input2');
         passwordInput.simulate('change', { target: { value: '123456' } });
 
         const data = {
             email: 'brij@hcl.com',
             password: '123456'
         }
-        const responseData = {data:{ message:'success',data: { } }};
+        const responseData = { data: { message: 'success', data: {} } };
         //mock.onPost('https://reqres.in/api/login').reply(200,responseData);
         mockedAxios.post.mockImplementationOnce(() => Promise.resolve(responseData))
 
@@ -66,7 +71,7 @@ describe('<Login />', () => {
         button.simulate('click');
 
         expect(mockedAxios.post).toHaveBeenCalledTimes(1);
-        expect(mockedAxios.post).toHaveBeenCalledWith('https://simple-login-signup-rest.herokuapp.com/api/user/login',data);
+        expect(mockedAxios.post).toHaveBeenCalledWith('https://simple-login-signup-rest.herokuapp.com/api/user/login', data);
 
     })
 
