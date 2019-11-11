e9ee613b66d84c492c8118bf02a9cb00b0c7d1d0
Kratika Kushwaha <kratika.kushwaha@hcl.com>
2019-11-11 13:11:27 +0530

    merge

--- src/components/organisms/RegistrationForm.tsx
+++ src/components/organisms/RegistrationForm.tsx
@@ -5,24 +5,25 @@ import InputText from "../atoms/InputText";
 import Head  from "../atoms/Head";
 import Field from "../molecules/Field";
 import image1 from "../../assests/image1.jpg";
-import Button from "../atoms/Button";
+import Button from "../atoms/Button"
 
 
-const FromContainer = styled.form`
+const FormContainer = styled.form`
+
   display: flex;
   text-align:center;
   align-items: center;
   flex-flow: column;
-  width: 300px;
-  height: 400px;
+  
   margin:  auto;
   border: 1px ;
   border-radius: 20px;
   background-repeat: no-repeat;
   background-image: url(${image1});
-  height:65vh;
+  height:70vh;
   width: 40vw;
-  margin-top:120px;
+
+
   background-size: cover;
   background-position: center center;
   box-sizing: border-box;
@@ -33,17 +34,20 @@ interface Props {
 }
 
 const RegistrationForm = (props: Props) => (
-    <FromContainer>
+    
+
+
+    <FormContainer >
+      
  <Head value={props.title}></Head>
-      <form >
       
         <Field labelValue="UserName" inputPlaceholder="UserName"></Field>
         <Field labelValue="Email" inputPlaceholder="Email"></Field>
         <Field labelValue="Password" inputPlaceholder="Password"></Field>
         <Field labelValue="Contact No" inputPlaceholder=""></Field>
-        <Button buttonText="submit"></Button>
-      </form>
-      </FromContainer>
-
+        <Button  buttonText="submit"></Button>
+        
+      </FormContainer>
+      
 );
 export default RegistrationForm;
